2035c0155db5d646ca28a1a8aba1c099d8646c51
Rune Mønnike <rm@gospoort.com>
2017-06-14 21:23:33 +0200

    Reordered table creations to suit FKs.

--- team_planner.sql
+++ team_planner.sql
@@ -4,6 +4,62 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table team_planner.skill
+CREATE TABLE IF NOT EXISTS `skill` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- Dumping data for table team_planner.skill: ~0 rows (approximately)
+/*!40000 ALTER TABLE `skill` DISABLE KEYS */;
+INSERT INTO `skill` (`id`, `name`) VALUES
+  (4, 'Architectural design'),
+  (1, 'Backend'),
+  (12, 'Cryptography'),
+  (7, 'Database design'),
+  (6, 'Devops'),
+  (14, 'Distrubuted system design'),
+  (2, 'Frontend'),
+  (3, 'Graphical design'),
+  (13, 'High performance computing'),
+  (9, 'Marketing'),
+  (11, 'Network security'),
+  (15, 'Payment gateways'),
+  (10, 'Print design'),
+  (5, 'Project management'),
+  (8, 'Video production');
+/*!40000 ALTER TABLE `skill` ENABLE KEYS */;
+
+-- Dumping structure for table team_planner.technology
+CREATE TABLE IF NOT EXISTS `technology` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table team_planner.technology: ~0 rows (approximately)
+/*!40000 ALTER TABLE `technology` DISABLE KEYS */;
+INSERT INTO `technology` (`id`, `name`) VALUES
+  (13, 'BreezeJS'),
+  (1, 'C#'),
+  (8, 'CSS'),
+  (11, 'd3.js'),
+  (12, 'EntityFramework'),
+  (7, 'HTML'),
+  (4, 'Java'),
+  (3, 'Javascript'),
+  (10, 'Less'),
+  (6, 'MSSQL'),
+  (5, 'MySQL'),
+  (2, 'PHP'),
+  (15, 'RabbitMQ'),
+  (14, 'Redis'),
+  (9, 'Sass');
+/*!40000 ALTER TABLE `technology` ENABLE KEYS */;
+
 -- Dumping structure for table team_planner.person
 CREATE TABLE IF NOT EXISTS `person` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -97,62 +153,6 @@ CREATE TABLE IF NOT EXISTS `project_skill` (
 -- Dumping data for table team_planner.project_skill: ~0 rows (approximately)
 /*!40000 ALTER TABLE `project_skill` DISABLE KEYS */;
 /*!40000 ALTER TABLE `project_skill` ENABLE KEYS */;
-
--- Dumping structure for table team_planner.skill
-CREATE TABLE IF NOT EXISTS `skill` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
-
--- Dumping data for table team_planner.skill: ~0 rows (approximately)
-/*!40000 ALTER TABLE `skill` DISABLE KEYS */;
-INSERT INTO `skill` (`id`, `name`) VALUES
-	(4, 'Architectural design'),
-	(1, 'Backend'),
-	(12, 'Cryptography'),
-	(7, 'Database design'),
-	(6, 'Devops'),
-	(14, 'Distrubuted system design'),
-	(2, 'Frontend'),
-	(3, 'Graphical design'),
-	(13, 'High performance computing'),
-	(9, 'Marketing'),
-	(11, 'Network security'),
-	(15, 'Payment gateways'),
-	(10, 'Print design'),
-	(5, 'Project management'),
-	(8, 'Video production');
-/*!40000 ALTER TABLE `skill` ENABLE KEYS */;
-
--- Dumping structure for table team_planner.technology
-CREATE TABLE IF NOT EXISTS `technology` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
-
--- Dumping data for table team_planner.technology: ~0 rows (approximately)
-/*!40000 ALTER TABLE `technology` DISABLE KEYS */;
-INSERT INTO `technology` (`id`, `name`) VALUES
-	(13, 'BreezeJS'),
-	(1, 'C#'),
-	(8, 'CSS'),
-	(11, 'd3.js'),
-	(12, 'EntityFramework'),
-	(7, 'HTML'),
-	(4, 'Java'),
-	(3, 'Javascript'),
-	(10, 'Less'),
-	(6, 'MSSQL'),
-	(5, 'MySQL'),
-	(2, 'PHP'),
-	(15, 'RabbitMQ'),
-	(14, 'Redis'),
-	(9, 'Sass');
-/*!40000 ALTER TABLE `technology` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
